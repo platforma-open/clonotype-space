fce50e5c39ce3cc38905f145e1598feebbe4f0b6
Mariia Zueva <maryzueva@gmail.com>
2026-02-02 16:11:13 +0100

    Fix default label derivation

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,6 +1,7 @@
+import { isJsonEqual } from '@milaboratories/helpers';
 import { getDefaultBlockLabel, model } from '@platforma-open/milaboratories.clonotype-space.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
-import { watchEffect } from 'vue';
+import { computed, watchEffect } from 'vue';
 import UmapPage from './pages/UmapPage.vue';
 
 export const sdkPlugin = defineApp(model, (app) => {
@@ -24,18 +25,21 @@ export const useApp = sdkPlugin.useApp;
 type AppModel = ReturnType<typeof useApp>['model'];
 
 function syncDefaultBlockLabel(model: AppModel) {
-  watchEffect(() => {
-    const sequenceLabels = model.args.sequencesRef
+  const sequenceLabels = computed(() => {
+    return model.args.sequencesRef
       .map((r) => {
         const label = model.outputs.sequenceOptions
-          ?.find((o) => o.value === r)
+          ?.find((o) => isJsonEqual(o.value, r))
           ?.label;
         return label ?? '';
       })
       .filter(Boolean)
       .sort();
+  });
+
+  watchEffect(() => {
     model.args.defaultBlockLabel = getDefaultBlockLabel({
-      sequenceLabels,
+      sequenceLabels: sequenceLabels.value,
       umap_neighbors: model.args.umap_neighbors,
       umap_min_dist: model.args.umap_min_dist,
     });
